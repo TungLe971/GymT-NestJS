9ee33d7ee73e72a62439ba86ef4d70a2d1931674
Tung <lmt3997@gmail.com>
2024-04-09 01:38:23 +0700

    fix card

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -1,56 +1,54 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseArrayPipe, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { CardService } from './card.service';
 import { Card } from './entities/card.entity';
+import { AuthGuard } from '../auth/auth.guard';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
 import { FilterCardDto } from './dto/filter-card.dto';
-import { AuthGuard } from 'src/auth/auth.guard';
-import { DeleteResult, UpdateResult } from 'typeorm';
 import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @ApiBearerAuth()
 @ApiTags('Cards')
 @Controller('cards')
 export class CardController {
-    constructor(private readonly cardService: CardService) {}
+    constructor(private cardService: CardService) {}
 
     @UseGuards(AuthGuard)
-    @ApiQuery({ name: 'page', type: Number, required: false })
-    @ApiQuery({ name: 'items_per_page', type: Number, required: false })
-    @ApiQuery({ name: 'search', type: String, required: false })
+    @ApiQuery({ name: 'page' })
+    @ApiQuery({ name: 'items_per_page' })
+    @ApiQuery({ name: 'search' })
     @Get()
-    async findAll(@Query() query: FilterCardDto): Promise<any> {
+    findAll(@Query() query: FilterCardDto): Promise<any> {
+        console.log(query);
         return this.cardService.findAll(query);
     }
 
     @UseGuards(AuthGuard)
     @Get(':id_card')
-    async findOne(@Param('id_card') id_card: string): Promise<Card> {
-        return this.cardService.findOne(+id_card);
+    findOne(@Param('id_card') id_card: string): Promise<Card> {
+        return this.cardService.findDetail(Number(id_card));
     }
 
     @UseGuards(AuthGuard)
-    @Post()
-    async create(@Body() createCardDto: CreateCardDto): Promise<Card> {
-        return this.cardService.create(createCardDto);
+    @Post(':hvId/:nvId/:packagesId')
+    create(@Param('hvId') hvId: string, @Param('nvId') nvId: string, @Param('packagesId') packagesId: string, @Body() createCardDto: CreateCardDto): Promise<Card> {
+        return this.cardService.create(Number(hvId), Number(nvId), Number(packagesId), createCardDto);
     }
 
     @UseGuards(AuthGuard)
-    @Patch(':id_card')
-    async update(@Param('id_card') id_card: string, @Body() updateCardDto: UpdateCardDto): Promise<UpdateResult> {
-        return this.cardService.update(+id_card, updateCardDto);
+    @Put(':id_card')
+    update(@Param('id_card') id_card: string, @Body() updateCardDto: UpdateCardDto) {
+        return this.cardService.update(Number(id_card), updateCardDto);
     }
 
-    @UseGuards(AuthGuard)
     @Delete(':id_card')
-    async delete(@Param('id_card') id_card: string): Promise<DeleteResult> {
-        return this.cardService.delete(+id_card);
+    delete(@Param('id_card') id_card: string) {
+        return this.cardService.delete(Number(id_card));
     }
 
-    @UseGuards(AuthGuard)
-    @Delete('multiple')
-    async deleteMultiple(@Query('id_cards') id_cards: string): Promise<DeleteResult> {
-        const id_cardsArray = id_cards.split(',').map(Number);
-        return this.cardService.deleteMultiple(id_cardsArray);
+    @Delete()
+    multipleDelete(@Query('id_cards', new ParseArrayPipe({ items: String, separator: ',' })) id_cards: string[]) {
+        console.log("delete multi=> ", id_cards)
+        return this.cardService.deleteMultiple(id_cards.map(Number));
     }
 }
